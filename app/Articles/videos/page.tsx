392b8e37fdeda5bf3e7032e806cde06efc5f3bfb
mohsalim <moharts2000@gmail.com>
2023-12-17 02:33:13 +0300

    all

--- app/Articles/videos/page.tsx
+++ app/Articles/videos/page.tsx
@@ -1,9 +1,13 @@
+import TransitionEffect from '../TransitionEffect';
 import Main from './Main';
 
 export default function Home() {
     return (
-        <main className="flex relative flex-col items-center px-4 snap-y snap-mandatory">
-            <Main />
-        </main>
+        <>
+            <TransitionEffect />
+            <main className="flex relative flex-col items-center px-4 snap-y snap-mandatory">
+                <Main />
+            </main>
+        </>
     );
 }
